6133f65f8bdbf81a93b3a623f6f02f71b34e2ccd
Moraswi Lekgothoane <phineas@softidoc.co.za>
2023-12-22 15:07:07 +0200

    vue add vuex

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: [
+//     'vuetify'
+//   ]
+// })
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: ["vuetify"],
+
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/styles.scss";`,
+      },
+    },
+  },
+});
