c24355c12e9f64020614f6b68d9be1c62cea2c2d
Moraswi Lekgothoane <phineas@softidoc.co.za>
2023-12-24 15:34:42 +0200

    skills

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -26,6 +26,11 @@
         <Experience />
       </section>
 
+      <!-- Experience -->
+      <section id="skills">
+        <Skills />
+      </section>
+
       <!-- Projects -->
       <section id="projects">
         <Projects />
@@ -45,6 +50,7 @@ import ProfessionalSummary from "@/components/Home/HomeComponents/ProfessionalSu
 import WhatIDo from "@/components/Home/HomeComponents/WhatIDo.vue";
 import Education from "@/components/Home/HomeComponents/Education.vue";
 import Experience from "@/components/Home/HomeComponents/Experience.vue";
+import Skills from "@/components/Home/HomeComponents/Skills.vue";
 import Projects from "@/components/Home/HomeComponents/Projects.vue";
 import Contacts from "@/components/Home/HomeComponents/Contacts.vue";
 
@@ -58,6 +64,7 @@ export default {
     WhatIDo,
     Education,
     Experience,
+    Skills,
     Projects,
     Contacts,
   },
---
+++ src/components/Home/HomeComponents/Skills.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="mt-5">
+    <!-- margin card -->
+    <!-- <v-card
+      class="transparent hidden-sm-and-down"
+      height="70"
+      flat
+      tile
+    ></v-card> -->
+    <p class="grey--text text2 mt-16">SKILLS</p>
+    <h2>SOME OF MY SKILLS</h2>
+
+    <v-row class="mt-3">
+      <v-col cols="12" md="3">
+        <ul>
+          <li>Flutter</li>
+          <li>Vue.js</li>
+          <li>React.Js</li>
+          <li>React Native</li>
+          <li>Vuetify</li>
+          <li>SQL</li>
+        </ul>
+      </v-col>
+
+      <v-col cols="12" md="3">
+        <ul>
+          <li>MySQL Workbench</li>
+          <li>Postman</li>
+          <li>HTML/CSS</li>
+          <li>jAVASCRIPT</li>
+          <li>Bootstrap</li>
+          <li>Version Control/Git</li>
+        </ul>
+      </v-col>
+
+      <v-col cols="12" md="3">
+        <ul>
+          <li>Angular</li>
+          <li>C#(begginner)</li>
+          <li>Selenium</li>
+          <li>UI/UX Design</li>
+          <li>Figma</li>
+          <li>REST APIs</li>
+          <li>Docker</li>
+        </ul>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SkillsSection",
+
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss" scoped>
+//
+.text2 {
+  font-family: "Krub", sans-serif;
+  font-size: 15px;
+}
+</style>
